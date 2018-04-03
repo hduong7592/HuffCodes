109c1f2fffa5a230358b7e3bf9eeab121fb6181d
hduong7592 <hieuduong08@gmail.com>
2018-04-03 11:59:54 -0400

    Update

--- src/HeapNode.java
+++ src/HeapNode.java
@@ -1,18 +1,38 @@
-/**
- * HeapNode class
- *
- * @author Hieu Duong
- * @date 4/2/18
- */
-
-public class HeapNode<T>{
+public class HeapNode<T> {
     double priority;
     T data;
+    HeapNode nodeLeft;
+    HeapNode nodeRight;
+
+    public HeapNode(){
+
+    }
+
+    public HeapNode(HeapNode nLeft, HeapNode nRight){
+        this.priority = nLeft.getP()+nRight.getP();
+        data = (T)(nLeft.getD()+""+nRight.getD());
+        this.nodeLeft = nLeft;
+        this.nodeRight = nRight;
+    }
+
     public HeapNode(T datum, double priority){
         this.priority=priority;
         data=datum;
     }
 
+    public HeapNode getNodeLeft() {
+        return nodeLeft;
+    }
+
+    public HeapNode getNodeRight() {
+        return nodeRight;
+    }
+
+    public boolean isLeaf(){
+        if(nodeLeft==null&& nodeRight==null){
+            return true;
+        }else return false;
+    }
     public double getP(){return priority;}
     public T getD(){return data;}
     public String toString(){
