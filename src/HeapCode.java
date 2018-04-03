c72635363fbac31853e4ec9323e2bbce4f66b32b
hduong7592 <hieuduong08@gmail.com>
2018-04-03 11:59:27 -0400

    New

---
+++ src/HeapCode.java
@@ -0,0 +1,17 @@
+public class HeapCode {
+    private String letter;
+    private String code;
+
+    public HeapCode(String letter, String code) {
+        this.letter = letter;
+        this.code = code;
+    }
+
+    public String getLetter() {
+        return letter;
+    }
+
+    public String getCode() {
+        return code;
+    }
+}
